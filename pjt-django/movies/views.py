df4442e973ac8852c7a3a558003de8887912520c
tarrot00 <tarrot00@gmail.com>
2022-05-19 17:44:24 +0900

    develope:index랑 base 제작

--- pjt-django/movies/views.py
+++ pjt-django/movies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
+
 # Create your views here.
+def index(request):
+    movies = []
+    context = {
+        'movies': movies,
+    }
+    return render (request, 'movies/index.html', context)
