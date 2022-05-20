6c4753de22e26ed0634911a9948cefba378ad85e
ohmy-Josh <jaesang95@gmail.com>
2022-05-20 13:17:38 +0900

    Feat : accounts, movies 앱 기본 구성

--- pjt-django/movies/urls.py
+++ pjt-django/movies/urls.py
@@ -5,4 +5,6 @@ app_name = 'movies'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:movie_pk>/', views.detail, name='detail'),
+    path('<int:movie_pk>/review/', views.create_review, name='create_review'),
 ]
--- pjt-django/movies/views.py
+++ pjt-django/movies/views.py
@@ -1,11 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_http_methods, require_GET, require_POST
+
+from .models import Movie, Actor, Director, Review
+from .forms import ReviewForm
 
 
-
-# Create your views here.
+@require_http_methods(['GET', 'POST'])
 def index(request):
-    movies = []
+    if request.method == 'POST':
+        # 관리자만 접근 가능 + 새로운 영화 목록 추가
+        return
+    else:
+        movies = Movie.objects.filter(pk__lte = 12)     # 반환할 movie list 수정 필요
     context = {
         'movies': movies,
     }
-    return render (request, 'movies/index.html', context)
+    
+    return render(request, 'movies/index.html', context)
+
+
+@require_GET
+def detail(request, movie_pk):
+    movie = get_object_or_404(Movie, pk = movie_pk)
+    
+    actors = movie.actors.all()[:4]
+    directors = movie.directors.all()[:2]
+    reviews = movie.review_set.all()
+
+    actor_movies, director_movies = Movie.objects.none(), Movie.objects.none()
+    for actor in actors:
+        actor_movies = actor_movies.union(actor.movies.all())
+    for director in directors:
+        director_movies = director_movies.union(director.movies.all())
+    
+    context = {
+        'movie': movie,
+        'actor_movies': actor_movies,
+        'director_movies': director_movies,
+        'reviews': reviews,
+    }
+    
+    return render(request, 'movies/detail.html', context)
+
+
+@require_POST
+def create_review(request, movie_pk):
+    # 로그인한 사용자만 접근하도록 허용하기
+
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        review = form.save(commit=False)
+        review.movie = movie
+        review.user = request.user
+        review.save()
+    return redirect('movies:detail', movie.pk)
+
+
+# review 수정 및 삭제 구현
