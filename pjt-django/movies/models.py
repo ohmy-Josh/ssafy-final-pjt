d9cf7faa82461813d5780b5c7daf869733088c0b
ohmy-Josh <jaesang95@gmail.com>
2022-05-20 23:01:11 +0900

    Feat : 모델 변경, 이름 한글화, 예고편 URL 기능 추가

--- pjt-django/movies/models.py
+++ pjt-django/movies/models.py
@@ -4,24 +4,24 @@ from django.conf import settings
 # Create your models here.
 class Actor(models.Model):
     name = models.CharField(max_length=100)
+    name_kr = models.CharField(max_length=100)
 
 
 class Director(models.Model):
     name = models.CharField(max_length=100)
+    name_kr = models.CharField(max_length=100)
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    overview = models.TextField()
-    backdrop_path = models.URLField()
-    poster_path = models.URLField()
+    backdrop_path = models.CharField(max_length=30)
+    poster_path = models.CharField(max_length=30)
+    trailer_path =  models.CharField(max_length=30)
     release_date = models.DateField()
     vote_average = models.FloatField()
+    overview = models.TextField()
     directors = models.ManyToManyField(Director, related_name='movies')
     actors = models.ManyToManyField(Actor, related_name='movies')
-    directors_kr = models.CharField(max_length=100)
-    actors_kr = models.CharField(max_length=100)
-    userRating = models.FloatField()
 
 
 class Review(models.Model):
