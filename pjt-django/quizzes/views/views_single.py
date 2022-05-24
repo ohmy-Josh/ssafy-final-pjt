c7ed6730de7593c6a576041dfe8348bdf194ac17
ohmy-Josh <jaesang95@gmail.com>
2022-05-24 11:18:17 +0900

    Feat : Quiz 모델 구축, single 모드 기능 추가

--- pjt-django/quizzes/views/views_single.py
+++ pjt-django/quizzes/views/views_single.py
@@ -3,43 +3,70 @@ from django.views.decorators.http import require_http_methods, require_GET, requ
 
 from movies.models import Movie, Actor, Director, Review
 from packages.movies import youtube, naver
-from packages.quizzes import quiz_list as Q
+from ..models import Quiz, QuizPlay
 
 @require_http_methods(['GET'])
 def index(request):
+    quizzes = Quiz.objects.all()
     context = {
-        'quiz_list': Q.quiz_list.values(),
+        'quizzes': quizzes,
     }
     return render(request, 'quizzes/single/index.html', context)
 
 
-def quiz_index(request, quiz_num):
-    quiz = Q.quiz_list.get(quiz_num, {})
-    
+def quiz_index(request, quiz_pk):
+    quiz = Quiz.objects.get(pk = quiz_pk)
     context = {
         'quiz': quiz,
     }
     return render(request, 'quizzes/single/quiz_index.html', context)
 
 
-@require_http_methods(['GET'])
-def quiz_play(request, quiz_num):
-    quiz = Q.quiz_list.get(quiz_num, {})
-    
-    movies = Movie.objects.filter(original_language = 'ko')
-    movies = movies.order_by('?')[:5]
-    
-    quiz_list = [
-        {'question':q[0], 'answer':a[0]} 
-        for q, a in zip(
-            movies.values_list(quiz['question']), 
-            movies.values_list(quiz['answer'])
-            )]
-    
+@require_http_methods(['POST', 'GET'])
+def quiz_play(request, quiz_pk, quiz_num):
+    quiz = Quiz.objects.get(pk = quiz_pk)
+    user = request.user
     base_url = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/'
-    context = {
-        'quiz_list': quiz_list,
-        'base_url': base_url,
-    }
 
+    if quiz_num == 0: 
+        if request.method == 'POST':
+            # 퀴즈 목록을 생성하자
+            movies = Movie.objects.filter(original_language = 'ko')
+            movies = movies.order_by('?')[:5]
+            
+            quiz_play = QuizPlay.objects.create(
+                quiz = quiz,
+                user = user,
+                current  = 0,
+            )
+            for movie in movies:
+                quiz_play.answer_list.add(movie)
+            
+            quiz_current = {
+                'question' : quiz_play.answer_list.all().values_list(quiz.question)[quiz_play.current ][0],
+                'answer' : quiz_play.answer_list.all().values_list(quiz.answer)[quiz_play.current][0]
+            }    
+        else:
+            # method 오류
+            return
+    else:
+        if quiz_num == 5:
+            quiz_play =  QuizPlay.objects.get(quiz = quiz, user = user)
+            quiz_play.delete()
+            return redirect('quizzes:sin_index')
+
+        quiz_play =  QuizPlay.objects.get(quiz = quiz, user = user)
+        quiz_play.current = quiz_num
+        quiz_play.save()
+        quiz_current = {
+                'question' : quiz_play.answer_list.all().values_list(quiz.question)[quiz_play.current][0],
+                'answer' : quiz_play.answer_list.all().values_list(quiz.answer)[quiz_play.current][0]
+            }
+
+    context = {
+                'base_url': base_url,
+                'quiz_current': quiz_current,
+                'quiz_pk': quiz_pk,
+                'current': quiz_play.current + 1,
+        }
     return render(request, 'quizzes/single/quiz.html', context)
--- pjt-django/tmp.py
+++ pjt-django/tmp.py
@@ -12,7 +12,6 @@ from movies.models import Movie, Actor, Director
 movies = Movie.objects.filter(original_language = 'ko')
 movies = movies.order_by('?')[:5]
 
-POSTER_BASE_URL = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/'
-quiz_list = [{'question':q[0], 'answer':a[0]}for q, a in zip(movies.values_list('backdrop_path'), movies.values_list('title'))]
+movie = Movie.objects.get(pk = 1)
 
-print(quiz_list)
+print(movies.values_list('title')[0])
